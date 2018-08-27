80209a53581424beeb81a858bfdb4ec7597fc04e
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-27 19:22:41 -0400

    Remove journal_entry_date, journal_entry_time, and content from schema, add journal_entry_content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180827202301) do
+ActiveRecord::Schema.define(version: 20180827231243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,8 @@ ActiveRecord::Schema.define(version: 20180827202301) do
   create_table "journal_entries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "journal_entry_date"
-    t.time "journal_entry_time"
-    t.text "content"
     t.bigint "user_id"
+    t.text "journal_entry_content"
     t.index ["user_id"], name: "index_journal_entries_on_user_id"
   end
 
